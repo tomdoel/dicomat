6f6a8e89d57c9b221e02ec7d3264ee54ebd614e0
Tom Doel <t.doel@ucl.ac.uk>
2015-08-03 13:13:01 +0100

    Added CoreMat dependencies

---
+++ External/coremat/CoreEndian.m
@@ -0,0 +1,17 @@
+classdef CoreEndian
+    % CoreEndian. Enumeration specifying whether a file format is little or big
+    %            endian
+    %
+    %     Licence
+    %     -------
+    %     Part of CoreMat. https://github.com/tomdoel/coremat
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+
+    enumeration
+        LittleEndian
+        BigEndian
+    end
+end
+
